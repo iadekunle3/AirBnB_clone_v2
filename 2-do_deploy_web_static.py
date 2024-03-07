e279991f8038bc743d2fdb060a5c7b22a3eb2583
DarrylNnon <darrylnnon@gmail.com>
2024-03-07 13:34:13 -0800

    complete

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,32 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
-from datetime import datetime
-from fabric.api import *
-import shlex
+"""
+Distribute archive to web servers
+"""
+from fabric.api import put, run, env
 import os
 
 
-env.hosts = ['35.231.33.237', '34.74.155.163']
-env.user = "ubuntu"
-
+env.hosts = ["54.86.24.142", "52.90.23.36"]
 
 def do_deploy(archive_path):
-    """ Deploys """
+    """
+    Distribute archive to web servers
+    """
     if not os.path.exists(archive_path):
         return False
+    filename = os.path.basename(archive_path)
+    tmp_tar = "/tmp/{}".format(filename)
+    name, ext = os.path.splitext(filename)
+    new_folder = "/data/web_static/releases/{}".format(name)
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
+        put(archive_path, tmp_tar)
+        run("mkdir -p {}".format(new_folder))
+        run("tar -xzf {} -C {}".format(tmp_tar, new_folder))
+        run("rm {}".format(tmp_tar))
+        run("mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("rm -rf {}/web_static".format(new_folder))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+        run("ln -s {} /data/web_static/current".format(new_folder))
         return True
     except:
         return False
